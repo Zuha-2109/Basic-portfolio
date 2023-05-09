3c8b5aa01322fe2c26be60332032604d375f60bd
Zuha-2109 <zoha.khundmiri2010@gmail.com>
2023-05-10 00:22:20 +0530

    First

---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import "../index.css"
+
+const Navbar = () => {
+
+    <nav className="navbar">
+        <h1> The DOJO Bolg </h1>
+        <div className="links">
+
+            <a href="#"> Home</a>
+            <a href="#"> Blog</a>
+            <a href="#"> Contact</a>
+            <a href="#"> Projects</a>
+        </div>
+    </nav>
+
+   
+}
+
+
+export default Navbar;
---
+++ src/Components/Section.js
@@ -0,0 +1,18 @@
+// import { Children } from "react";
+
+const Section = ({title, content, children}) => {
+
+    return(
+        <div className="section">  
+       
+        <h1> {title}</h1>
+        <p> 
+        
+        {content}
+        </p>
+
+        {children}
+        </div>
+    )
+}
+export default Section;
